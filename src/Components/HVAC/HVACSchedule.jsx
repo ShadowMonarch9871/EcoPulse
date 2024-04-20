2ffa35e713720268b480f6ffe9554d7fb28214c7
Ayush Pokhriyal <ayushpokhriyal21@gmail.com>
2024-04-20 12:27:51 +0530

    HVAC schedule done

--- src/Components/HVAC/HVACSchedule.jsx
+++ src/Components/HVAC/HVACSchedule.jsx
@@ -25,51 +25,53 @@ const HVACSchedule = () => {
     setSelectedDate(date);
   };
 
-  const [rows, setRows] = useState((JSON.parse(localStorage.getItem('rows')))||[
-    {
-      name: "Row 1",
-      time: "8:00AM-12:00PM",
-      temperature: "20°C",
-      humidity: "30%",
-    },
-    {
-      name: "Row 2",
-      time: "12:00PM-4:00PM",
-      temperature: "21°C",
-      humidity: "32%",
-    },
-    {
-      name: "Row 3",
-      time: "4:00PM-8:00PM",
-      temperature: "22°C",
-      humidity: "34%",
-    },
-    {
-      name: "Row 4",
-      time: "8:00PM-12:00AM",
-      temperature: "23°C",
-      humidity: "36%",
-    },
-    {
-      name: "Row 5",
-      time: "12:00AM-8:00AM",
-      temperature: "24°C",
-      humidity: "38%",
-    },
-  ]);
+  const [rows, setRows] = useState(
+    JSON.parse(localStorage.getItem("rows")) || [
+      {
+        name: "Row 1",
+        time: "8:00AM-12:00PM",
+        temperature: "20°C",
+        humidity: "30%",
+      },
+      {
+        name: "Row 2",
+        time: "12:00PM-4:00PM",
+        temperature: "21°C",
+        humidity: "32%",
+      },
+      {
+        name: "Row 3",
+        time: "4:00PM-8:00PM",
+        temperature: "22°C",
+        humidity: "34%",
+      },
+      {
+        name: "Row 4",
+        time: "8:00PM-12:00AM",
+        temperature: "23°C",
+        humidity: "36%",
+      },
+      {
+        name: "Row 5",
+        time: "12:00AM-8:00AM",
+        temperature: "24°C",
+        humidity: "38%",
+      },
+    ]
+  );
 
   const handleTemperatureChange = (event, index) => {
     const newRows = [...rows];
     newRows[index].temperature = event.target.value;
     setRows(newRows);
-    localStorage.setItem('rows', JSON.stringify(newRows));
+    localStorage.setItem("rows", JSON.stringify(newRows));
   };
-  
+
   const handleHumidityChange = (event, index) => {
     const newRows = [...rows];
     newRows[index].humidity = event.target.value;
     setRows(newRows);
-    localStorage.setItem('rows', JSON.stringify(newRows));
+    localStorage.setItem("rows", JSON.stringify(newRows));
   };
 
   return (
@@ -88,32 +90,42 @@ const HVACSchedule = () => {
       </LocalizationProvider>
       <TableContainer
         component={Paper}
-        style={{ backgroundColor: palette.grey[400]}}
+        style={{ backgroundColor: palette.grey[400] }}
       >
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell><h2>Time</h2></TableCell>
-              <TableCell align="right"><h2>Temperature(C)</h2></TableCell>
-              <TableCell align="right"><h2>Humidity(%)</h2></TableCell>
+              <TableCell>
+                <h2>Time</h2>
+              </TableCell>
+              <TableCell align="right">
+                <h2>Temperature(C)</h2>
+              </TableCell>
+              <TableCell align="right">
+                <h2>Humidity(%)</h2>
+              </TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {rows.map((row, index) => (
               <TableRow key={row.name}>
-                <TableCell component="th" scope="row">
+                <TableCell component="th" scope="row" style={{ fontWeight: 'bold' }}>
                   {row.time}
                 </TableCell>
                 <TableCell align="right">
                   <TextField
-                    value={row.temperature}
+                    type="number"
+                    value={parseInt(row.temperature)}
                     onChange={(event) => handleTemperatureChange(event, index)}
+                    inputProps={{ style: { width: "150px" } }}
                   />
                 </TableCell>
                 <TableCell align="right">
                   <TextField
-                    value={row.humidity}
+                    type="number"
+                    value={parseInt(row.humidity)}
                     onChange={(event) => handleHumidityChange(event, index)}
+                    inputProps={{ style: { width: "150px" } }}
                   />
                 </TableCell>
               </TableRow>
